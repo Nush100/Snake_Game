0b130a743db19bff459eafef16abaed8de62cb6f
nushara <nushkavi123@gmail.com>
2024-03-21 17:12:58 +0530

    first commit

---
+++ src/SnakeGame.java
@@ -0,0 +1,11 @@
+public class SnakeGame {
+
+    public static void main(String[] args) {
+         java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new GameFrame().setVisible(true);
+            }
+        });
+    }
+    
+}
